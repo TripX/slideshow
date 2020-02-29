b767e4fd9997cfc466610ddb2d10d5e8afb6452d
TripX <rouseau.bastien@gmail.com>
2020-02-29 23:46:29 +0100

    feat: add inversion of sorting by file date on a ugly button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, HostListener, OnInit} from '@angular/core';
 import {ElectronService} from "../core/services";
 import Timeout = NodeJS.Timeout;
+import {Dirent} from "fs";
 
 enum ESlideshowState {
   RUNNING,
@@ -33,6 +34,7 @@ export class HomeComponent implements OnInit {
   private slideShowState: ISlideshowState;
   private imagesSrc: IFile[];
   private indexImage: IIndexImage;
+  private filesDateOldToRecent: boolean;
 
   private intervalBeforeShowingImage: Timeout;
   private intervalSelectFolder: Timeout;
@@ -41,6 +43,7 @@ export class HomeComponent implements OnInit {
 
   constructor(public electronService: ElectronService) {
     this.imagesSrc = [];
+    this.filesDateOldToRecent = true;
     this.indexImage = {
       previous: 0,
       current: 1,
@@ -73,29 +76,43 @@ export class HomeComponent implements OnInit {
   }
 
   private startLoadingFolder(chosenFilePath: string) {
+
+    const pipe = (...fns: any[]) => (x: any) => fns.reduce((y, f) => f(y), x);
+
     const pathFolder = chosenFilePath.substring(0, chosenFilePath.lastIndexOf(this.electronService.pathSeparator) + 1);
-    this.imagesSrc = this.loadFolder(pathFolder);
+
+    const getFilesProperties = (dirents: Dirent[]): IFile[] => {
+      return dirents
+        .filter(item => !item.isDirectory())
+        .map(file => {
+          const fullPath = pathFolder + file.name;
+          return {
+            name: 'file:///' + fullPath,
+            time: this.electronService.fs.statSync(fullPath).mtime.getTime()
+          }
+        });
+    };
+
+    const sortFromOldToRecent = (files: IFile[]): IFile[] => {
+      return files.sort(((a, b) => a.time - b.time))
+    };
+
+    const sortFromRecentToOld = (files: IFile[]): IFile[] => {
+      return files.sort(((a, b) => b.time - a.time))
+    };
 
     if (this.intervalSelectFolder) {
       clearInterval(this.intervalSelectFolder);
     }
 
     this.intervalSelectFolder = setInterval(() => {
-      this.imagesSrc = this.loadFolder(pathFolder);
+      const loadedFiles = this.electronService.fs.readdirSync(pathFolder, {withFileTypes: true});
+      if (this.filesDateOldToRecent) {
+        this.imagesSrc = pipe(getFilesProperties, sortFromOldToRecent)(loadedFiles);
+      } else {
+        this.imagesSrc = pipe(getFilesProperties, sortFromRecentToOld)(loadedFiles);
+      }
     }, 500);
-  }
-
-  private loadFolder(pathFolder: string): IFile[] {
-    return this.electronService.fs.readdirSync(pathFolder, {withFileTypes: true})
-      .filter(item => !item.isDirectory())
-      .map(file => {
-        return {
-          name: 'file:///' + pathFolder + file.name,
-          time: this.electronService.fs.statSync(pathFolder + '/' + file.name).mtime.getTime()
-        }
-      })
-      .sort((a, b) => a.time - b.time)
-      .map((v) => v);
   }
 
   private stopSlideshow() {
@@ -166,5 +183,9 @@ export class HomeComponent implements OnInit {
 
   isSlideshowStopped() {
     return this.slideShowState.state === ESlideshowState.STOPPED;
+  }
+
+  inverseSorting() {
+    this.filesDateOldToRecent = !this.filesDateOldToRecent;
   }
 }
